47f87cb0b8fd54c3a19913ce4436907acd5173c4
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 20:23:33 -0700

    Create Jobsheet 9 C++.cpp
    program prulangan dengan for yang menampilkan angka x sampai n

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int x, n;
+	cout << "masukkan 2 angka terakhir nim asisten = ";
+	cin >> x;
+	cout << "masukkan 2 angka terakhir nim anda = ";
+	cin >> n;
+	for (int i=x; i<=n; i++) {
+		cout<<i<< endl;
+	}
+return 0;}
