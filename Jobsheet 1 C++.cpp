75ad30ec6b624323cd9a23c2861b8aebde9c42e7
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 20:04:50 -0700

    Create Jobsheet 1 C++.cpp
    program untuk menyeleksi status cum laude jika ipk untuk cum laude lebih besar atau sama dengan 3,50

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float ipk;
+cout<<"Masukan IPK Anda"<<endl;
+cin>>ipk;
+if(ipk>=3.50){
+	cout<<"SELAMAT ANDA CUMLAUDE";
+}
+else
+{
+cout<<"MOHON MAAF ANDA TIDAK CUMLAUDE";
+}
+return 0;	
+}
