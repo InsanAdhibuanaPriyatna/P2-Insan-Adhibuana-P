765246458dc00de4000dc147e230695edf48b873
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 20:05:36 -0700

    Create Jobsheet 2 C++.cpp
    program untuk mendapatkan bilangan yang lebih kecil dengan membandingkan dua bilangan

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int a,b,c;
+	cout<<"Masukkan bilangan 1 = "; 
+	cin>>a;
+	cout<<"Masukkan bilangan 2 = "; 
+	cin>>b;  
+	c=(a<b) ? a : b;
+	cout<< c <<" terkecil adalah "; 
+return 0;}
