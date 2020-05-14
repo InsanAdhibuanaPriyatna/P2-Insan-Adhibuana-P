04bcc78cfc9ed7feab29d7f5fcaadd3417d02242
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 20:26:57 -0700

    Create Jobsheet 12 C++.cpp
    program untuk menampilkan pasangan angka seperti dibawah
    (3,3),(3,2),(3,1),
    (2,3),(2,2),(2,1),
    (1,3),(1,2),(1,1),

---
+++ Jobsheet 12 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	for (int i = 3; i > 0; i--)
+	{
+	for (int j = 3; j > 0; j--)
+	{
+	cout << "("<<i<<","<< j << ") ";
+		}
+		cout << endl;
+	}
+return 0;}
