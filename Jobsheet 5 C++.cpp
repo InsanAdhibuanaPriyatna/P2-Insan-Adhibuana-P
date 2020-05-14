21de57c3ff69dcb86aff87b404f48a70689a0844
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 20:08:06 -0700

    Create Jobsheet 5 C++.cpp
    program untuk mengklasifikasikan index nilai, dimana diketahui kisaran index nilai A (85 – 100), B (75 – 84), C (60 – 74).

---
+++ Jobsheet 5 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+ 	int nilai;
+	cout<<"masukkan nilai = ";
+	cin>>nilai;
+	if ((nilai >= 85) && (nilai =100))
+	{
+		cout<<"Nilia : A";
+	}else if((nilai >= 75) && (nilai < 85))
+	{
+		cout<<"Nilai : B";
+	}else if((nilai >= 60) && (nilai < 75))
+	{
+		cout<<"Nilai: C";
+	}
+return 0;}
